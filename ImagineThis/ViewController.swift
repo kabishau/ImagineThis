ced32d3d0db9cb33d86f16c427704bdf112d599f
user139405 <user139405@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-11 18:51:11 -0500

    updates

--- ImagineThis/ViewController.swift
+++ ImagineThis/ViewController.swift
@@ -2,34 +2,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    @IBOutlet weak var sentenseLabel: UILabel!
-    
-    @IBAction func generateButtonTapped() {
-        sentenseLabel.text = generateRandom()
-    }
-    
-    func generateRandom() -> String {
-        let adjective = adjectives.randomElement()
-        let noun = nouns.randomElement()
-        let verb = verbs.randomElement()
-        let adverb = adverbs.randomElement()
+    @IBOutlet weak var collectionView: UICollectionView!
         
-        let sentanse = adjective! + " " + noun! + " " + verb! + " " + adverb!
-        return sentanse
-    }
+    @IBOutlet weak var categoryLabel: UILabel!
     
-    var nouns: [String] = ["vase","hospital", "view", "bit", "airport", "brick", "squirrel", "flame", "toothpaste", "nose", "teeth", "apparel", "balance", "belief", "approval", "veil", "hand", "cat", "air", "robin", "wrench", "oven"]
-    
-    var verbs: [String] = ["wishes","invites", "decays", "strokes", "forces", "balance", "closes", "arrests", "burns", "dislikes", "dances", "reduces", "reflects", "refuses", "suspends", "smokes", "produces", "prints", "bleaches", "scares"]
-    
-    var adjectives: [String] = ["ruddy","nonstop", "fortunate", "steady", "unaccountable", "ashamed", "sticky", "phobic", "purring", "ablaze", "lying", "impartial", "damaging", "busy", "black", "alive", "fixed", "empty", "nasty", "tacky", "loose", "complete", "learned", "assorted"]
-    
-    var adverbs: [String] = ["knowledgeably","violently", "fervently", "rarely", "enormously", "wildly", "thoughtfully", "mortally", "upside-down", "initially", "arrogantly", "softly", "slowly", "urgently", "nicely", "blissfully", "closely", "tensely", "viciously", "bashfully", "merely", "recently", "voluntarily", "oddly"]
-    
+    var categories = ["urban", "horror", "news", "action", "fantasy", "scifi"]
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let width = (view.frame.size.width - (2 * 40 + 20)) / 2
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
 
     }
 }
 
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath)
+        if let label = cell.viewWithTag(100) as? UILabel {
+            label.text = categories[indexPath.row]
+        }
+        return cell
+    }
+}
+
+/*
+ 
+ func generateRandom() -> String {
+ let adjective = adjectives.randomElement()
+ let noun = nouns.randomElement()
+ let verb = verbs.randomElement()
+ let adverb = adverbs.randomElement()
+ 
+ let sentanse = adjective! + " " + noun! + " " + verb! + " " + adverb!
+ return sentanse
+ }
+ 
+var nouns: [String] = ["vase","hospital", "view", "bit", "airport", "brick", "squirrel", "flame", "toothpaste", "nose", "teeth", "apparel", "balance", "belief", "approval", "veil", "hand", "cat", "air", "robin", "wrench", "oven"]
+
+var verbs: [String] = ["wishes","invites", "decays", "strokes", "forces", "balance", "closes", "arrests", "burns", "dislikes", "dances", "reduces", "reflects", "refuses", "suspends", "smokes", "produces", "prints", "bleaches", "scares"]
+
+var adjectives: [String] = ["ruddy","nonstop", "fortunate", "steady", "unaccountable", "ashamed", "sticky", "phobic", "purring", "ablaze", "lying", "impartial", "damaging", "busy", "black", "alive", "fixed", "empty", "nasty", "tacky", "loose", "complete", "learned", "assorted"]
+
+var adverbs: [String] = ["knowledgeably","violently", "fervently", "rarely", "enormously", "wildly", "thoughtfully", "mortally", "upside-down", "initially", "arrogantly", "softly", "slowly", "urgently", "nicely", "blissfully", "closely", "tensely", "viciously", "bashfully", "merely", "recently", "voluntarily", "oddly"]
+*/
